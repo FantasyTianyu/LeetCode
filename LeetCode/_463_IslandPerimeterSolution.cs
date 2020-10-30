7b289877cc924adc13eef43f8a7cafad4ff929d3
FantasyTianyu <>
2020-10-30 15:41:46 +0800

    463

---
+++ LeetCode/_463_IslandPerimeterSolution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace LeetCode
+{
+    public class _463_IslandPerimeterSolution
+    {
+        public _463_IslandPerimeterSolution()
+        {
+        }
+
+        public int IslandPerimeter(int[][] grid)
+        {
+            int landNum = 0;
+            int linkNum = 0;
+            for (int x = 0; x < grid.Length; x++)
+            {
+                for (int y = 0; y < grid[0].Length; y++)
+                {
+                    if (grid[x][y] == 1)
+                    {
+                        landNum++;
+                        if (x < grid.Length - 1 && grid[x + 1][y] == 1)
+                        {
+                            linkNum++;
+                        }
+
+                        if (y < grid[0].Length - 1 && grid[x][y + 1] == 1)
+                        {
+                            linkNum++;
+                        }
+                    }
+                }
+            }
+            return landNum * 4 - linkNum * 2;
+        }
+
+
+    }
+}
